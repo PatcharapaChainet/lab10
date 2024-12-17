acb26a5a36217ae49bedebe5f8107ab6400345aa
PatcharapaChainet <patcharapa_chainet@cmu.ac.th>
2024-12-17 14:47:24 +0700

    lab 10 EZ

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,13 +6,17 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
+	string xd;
+	cout << "-------------------- BOOM ---------------------" << "\n";
+	while(getline(source,xd)) {
+	    cout << xd << endl;
+	}
+	cout << "-------------------- HA!! ---------------------";
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
     source.close();
     dest.close();
 	return 0;
 }
+
